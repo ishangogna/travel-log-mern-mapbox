29423734e6b93f6e5fb67958b4559706da4057c7
Ishan Gogna <ishangogna10@gmail.com>
2020-07-21 19:33:14 +0530

    boiler plate npm init and route setup

---
+++ server/src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = require('../routes/api');
+require('dotenv').config();
+
+
+const app = express();
+
+app.use(express.json());
+
+app.use(router);
+
+app.listen(process.env.PORT,()=>{
+    console.log(`listening @ ${process.env.PORT}`)
+})
+
