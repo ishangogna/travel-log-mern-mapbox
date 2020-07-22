9a2999ba63621ade5bdfbb569b2b5b15b6a8c50f
Ishan Gogna <ishangogna10@gmail.com>
2020-07-22 12:27:49 +0530

    added markers

--- client/src/components/map.js
+++ client/src/components/map.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL, { Marker } from 'react-map-gl';
  
 class Map extends Component {
  
@@ -32,7 +32,25 @@ class Map extends Component {
         mapboxApiAccessToken = "pk.eyJ1IjoiaXNoYW5nb2duYSIsImEiOiJjano4NHc2Z3cwMGZqM2Ruazlmdm14Mm9rIn0.W1PryFGxgSy4ftdgYTQ6iw"
         {...this.state.viewport}
         onViewportChange={(viewport) => this.setState({viewport})}
-      />
+      >
+        {this.state.logs.map(log=>(
+            <Marker key = {log._id} latitude={log.latitude} longitude={log.longitude} offsetLeft={-20} offsetTop={-10}>
+            <svg xmlns="http://www.w3.org/2000/svg" 
+                width="24" 
+                height="24" 
+                viewBox="0 0 24 24" 
+                fill="red" 
+                stroke="currentColor" 
+                strokeWidth="2" 
+                strokeLinecap="round" 
+                strokeLinejoin="round" 
+                className="feather feather-map-pin">
+                    <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
+                    <circle cx="12" cy="10" r="3"></circle>
+            </svg>
+            </Marker>
+        ))}
+      </ReactMapGL>
     );
   }
 }
