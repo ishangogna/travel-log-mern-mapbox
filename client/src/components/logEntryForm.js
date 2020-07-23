620ce1c98048aae94d741de5f6e59b323b164306
Ishan Gogna <ishangogna10@gmail.com>
2020-07-23 12:13:45 +0530

    logEntry/display logs pops on the map

---
+++ client/src/components/logEntryForm.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+
+
+const LogEntry = ( { longitude, latitude } ) => {
+    
+    const addLog = (e) => {
+        e.preventDefault();
+        const url = 'http://localhost:2000/api';
+        fetch(url,{
+            method : 'POST',
+            headers : {
+                'content-type' : 'application/json'
+            },
+            body : JSON.stringify({
+                title : title,
+                comments : comments,
+                longitude : longitude,
+                latitude : latitude
+            })
+        })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(err=> console.log(err));
+    
+    }
+
+    const [title, setTitle] = useState('');
+    const [comments, setComments] = useState('');
+    return ( 
+        <form style = {{display : 'flex', flexDirection : 'column'}}>
+                            <input 
+                                type="text" 
+                                placeholder = "Name of the location" 
+                                onChange = {(e)=> setTitle(e.target.value)} 
+                            />
+                            <textarea 
+                                type="text" 
+                                rows= {5} 
+                                placeholder = "Comments"
+                                onChange = {(e)=> setComments(e.target.value)} 
+
+                            />
+                            <button onClick = {addLog}>Add a log!</button>
+                        </form>
+     );
+}
+ 
+export default LogEntry;
--- client/src/components/map.js
+++ client/src/components/map.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import ReactMapGL, { Marker } from 'react-map-gl';
+import ReactMapGL, { Marker, Popup } from 'react-map-gl';
+import LogEntry from './logEntryForm';
  
 class Map extends Component {
  
@@ -11,7 +12,11 @@ class Map extends Component {
       longitude: 79.0882,
       zoom: 4
     },
-    logs : []
+    logs : [],
+    showPopup : {},
+    newPopup : {
+        longitude : 0,
+        latitude : 0,    }
   };
 
   componentDidMount = () => {
@@ -25,31 +30,93 @@ class Map extends Component {
             console.log(this.state.logs);
         });
   }
- 
+
+  addNewLog = (e) => {
+      const [ longitude, latitude ] = e.lngLat;
+      this.setState({newPopup : {longitude, latitude}});
+  }
+
   render() {
     return (
       <ReactMapGL
+        onDblClick = {this.addNewLog}
         mapboxApiAccessToken = "pk.eyJ1IjoiaXNoYW5nb2duYSIsImEiOiJjano4NHc2Z3cwMGZqM2Ruazlmdm14Mm9rIn0.W1PryFGxgSy4ftdgYTQ6iw"
         {...this.state.viewport}
         onViewportChange={(viewport) => this.setState({viewport})}
-      >
+      >        
         {this.state.logs.map(log=>(
-            <Marker key = {log._id} latitude={log.latitude} longitude={log.longitude} offsetLeft={-20} offsetTop={-10}>
-            <svg xmlns="http://www.w3.org/2000/svg" 
-                width="24" 
-                height="24" 
-                viewBox="0 0 24 24" 
-                fill="red" 
-                stroke="currentColor" 
-                strokeWidth="2" 
-                strokeLinecap="round" 
-                strokeLinejoin="round" 
-                className="feather feather-map-pin">
-                    <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
-                    <circle cx="12" cy="10" r="3"></circle>
-            </svg>
-            </Marker>
+            <div key = {log._id} onClick = {()=>{this.setState({showPopup : {[log._id] : true}})}}>
+                <Marker latitude={log.latitude} longitude={log.longitude} offsetLeft={-20} offsetTop={-10}>
+                    <svg xmlns="http://www.w3.org/2000/svg" 
+                        width="24" 
+                        height="24" 
+                        viewBox="0 0 24 24" 
+                        fill="red" 
+                        stroke="currentColor" 
+                        strokeWidth="2" 
+                        strokeLinecap="round" 
+                        strokeLinejoin="round" 
+                        className="feather feather-map-pin"
+                        >
+                            <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
+                            <circle cx="12" cy="10" r="3"></circle>
+                    </svg>
+                </Marker>
+                { this.state.showPopup[log._id] ? (
+                    <Popup
+                    latitude={log.latitude} 
+                    longitude={log.longitude}
+                    closeButton={true}
+                    closeOnClick={true}
+                    onClose={() => this.setState({showPopup: false})}
+                    anchor="top" >
+                    <div>
+                        <h3>{log.title}</h3>
+                        <p>{log.comments}</p>
+                    </div>
+                </Popup>
+                ) : null}
+                
+            </div>
         ))}
+        {
+            this.state.newPopup.longitude > 0 ? (
+                <div>
+                    <Marker latitude={this.state.newPopup.latitude} longitude={this.state.newPopup.longitude} offsetLeft={-20} offsetTop={-10}>
+                        <svg xmlns="http://www.w3.org/2000/svg" 
+                            width="24" 
+                            height="24" 
+                            viewBox="0 0 24 24" 
+                            fill="red" 
+                            stroke="currentColor" 
+                            strokeWidth="2" 
+                            strokeLinecap="round" 
+                            strokeLinejoin="round" 
+                            className="feather feather-map-pin"
+                            >
+                                <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
+                                <circle cx="12" cy="10" r="3"></circle>
+                        </svg>
+                    </Marker>  
+                    <Popup
+                    latitude={this.state.newPopup.latitude} 
+                    longitude={this.state.newPopup.longitude}
+                    closeButton={true}
+                    closeOnClick={false}
+                    onClose={() => this.setState({newPopup: {longitude : 0, latitude : 0, name : '', comments : ''}})}
+                    anchor="top" >
+                    <div>
+                        <h3 style = {{textAlign : 'center'}}>Add a new Log!</h3>
+                        <LogEntry 
+                            longitude= {this.state.newPopup.longitude}
+                            latitude = {this.state.newPopup.latitude}
+                            />
+                    </div>
+                </Popup>
+
+                </div>
+            ) : null
+        }
       </ReactMapGL>
     );
   }
